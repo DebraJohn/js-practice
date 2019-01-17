4b5f1b0de8f0d0bf82577efca45d3e2b115d0448
DebraJohn <debra.zhuang@zeasn.com>
2019-01-17 18:24:57 +0800

    增加播放进度、切换歌曲功能

--- item-prac/MusicPlayer/js/index.js
+++ item-prac/MusicPlayer/js/index.js
@@ -8,8 +8,14 @@ const $prevBtn = $('.icon-prev');
 const $nextBtn = $('.icon-next');
 const $playBtn = $('.icon-play');
 const $dynamic = $('#dynamic');
+const $totalTime = $('.total-time');
+const $curTime = $('.cur-time');
+const $progressing = $('.progressing');
 let curRotate = 0;
 let style = '';
+let currentTime = 0;
+let playTimer = null;
+let progressRate = 0;
 
 let playStatus = {
   curIndex: 0,
@@ -20,47 +26,35 @@ let curSong = playList[playStatus.curIndex];
 playerInit();
 
 let mediaControl = {
-  prev: function() {
-    playStatus.curIndex = Math.min(0, playStatus.curIndex - 1);
-    playStatus.state = 1;
-  },
-  next: function() {
-    playStatus.curIndex = Math.max(
-      playList.length - 1,
-      playStatus.curIndex + 1
+  switchSong: function(state) {
+    // 0为上一首, 1为下一首
+    if (state) {
+      if (playStatus.curIndex < playList.length) {
+        playStatus.curIndex++;
+      }
+    } else {
+      if (playStatus.curIndex > 0) {
+        playStatus.curIndex--;
+        this.switchSong();
+      }
+    }
+    curSong = playList[playStatus.curIndex];
+    setPlayStatus(
+      curSong.albumPic,
+      curSong.songTitle,
+      curSong.singer,
+      curSong.url,
+      formatDurtion(curSong.duration)
     );
-    playStatus.state = 1;
+    this.resetPlayStatus();
+    setTimeout(() => {
+      this.__play__();
+    }, 500);
   },
   __play__: function() {
     playStatus.state = 1;
     $playBtn.find('img').attr('src', './images/pause.png');
-  },
-  __pause__: function() {
-    playStatus.state = 0;
-    $playBtn.find('img').attr('src', './images/play.png');
-  }
-};
-
-function playerInit() {
-  setPlayStatus(
-    curSong.albumPic,
-    curSong.songTitle,
-    curSong.singer,
-    curSong.url
-  );
-}
-
-function setPlayStatus(pic, title, singer, url) {
-  $albumPic.attr('src', pic);
-  $songTitle.html(title);
-  $singer.html(singer);
-  $musicLink.attr('src', url);
-}
-
-$playBtn.click(function() {
-  if (!playStatus.state) {
-    mediaControl.__play__();
-    // $musicLink[0].play();
+    progressBar(1);
     styles = `
       @-webkit-keyframes spin {
       from {
@@ -72,42 +66,82 @@ $playBtn.click(function() {
     }`;
     $dynamic.html(styles);
     $albumPic.addClass('spinning');
-  } else {
-    mediaControl.__pause__();
-    // $musicLink[0].pause();
+  },
+  __pause__: function() {
+    playStatus.state = 0;
+    $playBtn.find('img').attr('src', './images/play.png');
+    progressBar(0);
     curRotate = eval('get' + $albumPic.css('transform'));
     $albumPic
       .removeClass('spinning')
       .css('transform', `rotate(${curRotate}deg)`);
+  },
+  resetPlayStatus: function() {
+    currentTime = 0;
+    $curTime.html('00:00');
+    progressRate = 0;
+    $progressing.css('width', '0');
+    curRotate = 0;
+    $albumPic
+      .removeClass('spinning')
+      .css('transform', `rotate(${curRotate}deg)`);
+    clearInterval(playTimer);
+  }
+};
+
+function playerInit() {
+  setPlayStatus(
+    curSong.albumPic,
+    curSong.songTitle,
+    curSong.singer,
+    curSong.url,
+    formatDurtion(curSong.duration)
+  );
+}
+
+function progressBar(state) {
+  if (state) {
+    playTimer = setInterval(() => {
+      currentTime += 1000;
+      $curTime.html(formatDurtion(currentTime));
+      progressRate = (currentTime / curSong.duration) * 100;
+      $progressing.css('width', `${progressRate}%`);
+      currentTime >= Math.floor(curSong.duration / 1000) * 1000 &&
+        mediaControl.__pause__();
+    }, 1000);
+  } else {
+    clearInterval(playTimer);
+  }
+}
+function formatDurtion(time) {
+  function addZero(t) {
+    return (t = t < 10 ? `0${t}` : `${t}`);
+  }
+  const min = addZero(Math.floor(time / 1000 / 60));
+  const sec = addZero(Math.floor(time / 1000) - min * 60);
+  return `${min}:${sec}`;
+}
+
+function setPlayStatus(pic, title, singer, url, time) {
+  $albumPic.attr('src', pic);
+  $songTitle.html(title);
+  $singer.html(singer);
+  $musicLink.attr('src', url);
+  $totalTime.html(time);
+}
+
+$playBtn.click(function() {
+  if (!playStatus.state) {
+    mediaControl.__play__();
+  } else {
+    mediaControl.__pause__();
   }
 });
 
 $nextBtn.click(function() {
-  if (playList.length > playStatus.curIndex + 1) {
-    playStatus.curIndex = playStatus.curIndex + 1;
-    curSong = playList[playStatus.curIndex];
-    setPlayStatus(
-      curSong.albumPic,
-      curSong.songTitle,
-      curSong.singer,
-      curSong.url
-    );
-  }
+  mediaControl.switchSong(1);
 });
 
-function getmatrix(a, b, c, d, e, f) {
-  var aa = Math.round((180 * Math.asin(a)) / Math.PI);
-  var bb = Math.round((180 * Math.acos(b)) / Math.PI);
-  var cc = Math.round((180 * Math.asin(c)) / Math.PI);
-  var dd = Math.round((180 * Math.acos(d)) / Math.PI);
-  var deg = 0;
-  if (aa == bb || -aa == bb) {
-    deg = dd;
-  } else if (-aa + bb == 180) {
-    deg = 180 + cc;
-  } else if (aa + bb == 180) {
-    deg = 360 - cc || 360 - dd;
-  }
-  return deg >= 360 ? 0 : deg;
-  //return (aa+','+bb+','+cc+','+dd);
-}
+$prevBtn.click(function() {
+  mediaControl.switchSong(0);
+});
---
+++ item-prac/MusicPlayer/js/tools.js
@@ -0,0 +1,15 @@
+function getmatrix(a, b, c, d, e, f) {
+  var aa = Math.round((180 * Math.asin(a)) / Math.PI);
+  var bb = Math.round((180 * Math.acos(b)) / Math.PI);
+  var cc = Math.round((180 * Math.asin(c)) / Math.PI);
+  var dd = Math.round((180 * Math.acos(d)) / Math.PI);
+  var deg = 0;
+  if (aa == bb || -aa == bb) {
+    deg = dd;
+  } else if (-aa + bb == 180) {
+    deg = 180 + cc;
+  } else if (aa + bb == 180) {
+    deg = 360 - cc || 360 - dd;
+  }
+  return deg >= 360 ? 0 : deg;
+}
