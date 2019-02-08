b26a520374e406c93900ba340abcf989e3adc668
BenBonBen <944346264@qq.com>
2019-02-08 17:45:30 +0800

    添加随机播放功能

--- item-prac/MusicPlayer/js/index.js
+++ item-prac/MusicPlayer/js/index.js
@@ -1,16 +1,20 @@
 console.log(playList);
 const styleSheet = $('#indexStyleSheet');
-const $albumPic = $('.album_pic>img');//歌曲图片
-const $songTitle = $('.song_title');//歌名
-const $singer = $('.singer');//歌手
-const $musicLink = $('#music');//音乐链接
+const $albumPic = $('.album_pic>img'); //歌曲图片
+const $songTitle = $('.song_title'); //歌名
+const $singer = $('.singer'); //歌手
+const $musicLink = $('#music'); //音乐链接
 //获取按钮
 const $prevBtn = $('.icon-prev');
 const $nextBtn = $('.icon-next');
 const $playBtn = $('.icon-play');
 
+const $palystyles = $('.playstyles'); //获取播放方式
+const $orderPlay = $('.orderPlay'); //获取顺序播放
+const $randomPlay = $('.randomPlay'); //获取随机播放
+
 const $dynamic = $('#dynamic');
-const $totalTime = $('.total-time');//歌曲总时间
+const $totalTime = $('.total-time'); //歌曲总时间
 const $curTime = $('.cur-time');
 const $progressing = $('.progressing');
 
@@ -19,6 +23,7 @@ let style = '';
 let currentTime = 0;
 let playTimer = null;
 let progressRate = 0;
+let playtype = 0; //存储播放方式，0是顺序播放，1是随机播放
 
 //播放状态对象，里面包含curIndex（记录当前的歌曲下标），state（播放状态）
 let playStatus = {
@@ -33,20 +38,29 @@ playerInit();
 //媒体控制对象它下面有四个属性，
 //分别是switchSong(切换歌曲) ，play(播放) ，pause(暂停) ，resetPlayStatus（重置播放状态）
 let mediaControl = {
+  //切歌
   switchSong: function(state) {
     // 0为上一首, 1为下一首
-    if (state) {
+    if (state == 1) {
       if (playStatus.curIndex < playList.length - 1) {
         playStatus.curIndex++;
       } else {
         playStatus.curIndex = 0;
       }
+      // alert(2);
+    } else if (state == 3) {
+      let tempIndex = playStatus.curIndex;
+      do {
+        playStatus.curIndex = Math.floor(Math.random() * playList.length);
+      } while (tempIndex == playStatus.curIndex);
+      console.log(playStatus.curIndex);
     } else {
       if (playStatus.curIndex > 0) {
         playStatus.curIndex--;
       } else {
         playStatus.curIndex = playList.length - 1;
       }
+      console.log(playStatus.curIndex);
     }
     curSong = playList[playStatus.curIndex];
     setPlayStatus(
@@ -61,6 +75,7 @@ let mediaControl = {
       this.__play__();
     }, 500);
   },
+  //播放
   __play__: function() {
     playStatus.state = 1;
     $playBtn.find('img').attr('src', './images/pause.png');
@@ -78,6 +93,7 @@ let mediaControl = {
     $albumPic.addClass('spinning');
     $musicLink[0].play();
   },
+  //暂停
   __pause__: function() {
     playStatus.state = 0;
     $playBtn.find('img').attr('src', './images/play.png');
@@ -99,7 +115,7 @@ let mediaControl = {
       .removeClass('spinning')
       .css('transform', `rotate(${curRotate}deg)`);
     clearInterval(playTimer);
-  }
+  },
 };
 
 function playerInit() {
@@ -121,8 +137,13 @@ function progressBar(state) {
       progressRate = (currentTime / curSong.duration) * 100;
       $progressing.css('width', `${progressRate}%`);
       if (currentTime >= Math.floor(curSong.duration / 1000) * 1000) {
-        mediaControl.__pause__();
-        mediaControl.switchSong(1);
+        if (playtype == 0) {
+          mediaControl.__pause__();
+          mediaControl.switchSong(1);
+        } else {
+          mediaControl.__pause__();
+          mediaControl.switchSong(3);
+        }
       }
       //监听是否是最后一首，如果是，播放完就暂停
       // if (playStatus.curIndex = playList.length - 1 || currentTime >= Math.floor(curSong.duration / 1000) * 1000 ) {
@@ -139,7 +160,7 @@ function formatDurtion(time) {
   function addZero(t) {
     return (t = t < 10 ? `0${t}` : `${t}`);
   }
-  const min = addZero(Math.floor(time / 1000 / 60));//math.floor(x)返回小于等于x的最大整数
+  const min = addZero(Math.floor(time / 1000 / 60)); //math.floor(x)返回小于等于x的最大整数
   const sec = addZero(Math.floor(time / 1000) - min * 60);
   return `${min}:${sec}`;
 }
@@ -163,11 +184,34 @@ $playBtn.click(function() {
 });
 
 //添加下一首按钮点击的监听
-$nextBtn.click(function() {
-  mediaControl.switchSong(1);
+$nextBtn.click(function () {
+  if (playtype == 0) {
+    mediaControl.switchSong(1);
+  } else {
+    mediaControl.switchSong(3);
+  }
 });
 
 //添加上一首按钮点击的监听
 $prevBtn.click(function() {
-  mediaControl.switchSong(0);
+  if (playtype == 0) {
+    mediaControl.switchSong(0);
+  } else {
+    mediaControl.switchSong(3);
+  }
+});
+
+//添加随机播放与顺序播放切换的监听
+$palystyles.click(function() {
+  if ($randomPlay.css('display') == 'none') {
+    $randomPlay.removeClass('current');
+    $orderPlay.addClass('current');
+    playtype = 1;//1是随机播放
+    // console.log(playtype);
+  } else {
+    $orderPlay.removeClass('current');
+    $randomPlay.addClass('current');
+    playtype = 0;//0是顺序播放
+    // console.log(playtype);
+  }
 });
